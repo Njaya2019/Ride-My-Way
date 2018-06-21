81f169b717535dce4e8242601ab02a894598e6e5
Andrew <njayaandrew@gmail.com>
2018-06-21 16:55:15 +0300

    added test file

---
+++ test_ridemyway.py
@@ -0,0 +1,13 @@
+import json
+import pytest
+from ridemywayEp import app
+
+@pytest.fixture
+def client(request):
+    test_client=app.test_client()
+    return test_client
+
+def test_viewRides(client):
+    response=client.get('/rides')
+    assert response.sttus_code==200
+    
