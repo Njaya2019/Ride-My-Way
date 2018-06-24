a2f1d8fba1b9d14bd1b39b90371e5f9a05960256
Andrew <njayaandrew@gmail.com>
2018-06-24 13:18:08 +0300

    Added methods

--- app.py
+++ app.py
@@ -2,27 +2,27 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    return render_template('index.html',methods=["GET","POST"])
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html',methods=["GET","POST"])
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('signup.html',methods=["GET","POST"])
 
 @app.route('/driver')
 def driver():
-    return render_template('driver.html')
+    return render_template('driver.html', methods=["GET","POST"])
 
 @app.route('/rides')
 def rides():
-    return render_template('rides.html')
+    return render_template('rides.html', methods=["GET","POST"])
 
-@app.route('/passengers')
+@app.route('/passengers', methods=["GET","POST"])
 def passengers():
     return render_template('passengers.html')
 
